f0a90588c3435dab30521de27b0d1749ec1509f0
Himanshu Phoolwar <mr.coder.himanshu@gmail.com>
2022-03-14 23:07:08 +0530

    added terraform section

--- GCP-Projects/main.tf
+++ GCP-Projects/main.tf
@@ -1,5 +1,6 @@
 resource "google_project" "projects" {
-  for_each   = toset(var.project_list)
-  name       = each.key
-  project_id = lookup(var.project_id_list, each.key)
+  for_each        = toset(var.project_list)
+  name            = each.key
+  project_id      = lookup(var.project_id_list, each.key)
+  billing_account = "0158EE-90B086-A7AB6B"
 }
